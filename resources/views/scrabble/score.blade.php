1bb2d94fb6b007900f8c80899fa7b7f944c68d05
smaislin186 <samantha.maislin@gmail.com>
2017-04-10 22:27:22 -0400

    html fixes - radio button names

--- resources/views/scrabble/score.blade.php
+++ resources/views/scrabble/score.blade.php
@@ -25,17 +25,17 @@
                             <div class="radio-inline">
                                 <input type='radio' id='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' name='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'
                                     value='None' checked='CHECKED'>
-                                <label for='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' name='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'>None</label>
+                                <label for='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'>None</label>
                             </div>
                             <div class="radio-inline">
                                 <input type='radio' id='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' name='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'
                                     value='Double'>
-                                <label for='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' name='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'>Double</label>
+                                <label for='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' >Double</label>
                             </div>
                             <div class="radio-inline">
                                 <input type='radio' id='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' name='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'
                                     value='Triple'>
-                                <label for='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' name='bonusLetter[@php print $key @endphp][@php print $letter @endphp]'>Triple</label>
+                                <label for='bonusLetter[@php print $key @endphp][@php print $letter @endphp]' >Triple</label>
                             </div>  
                         </div>
                     @endforeach
